e5dee8fb19c73086f6f846704630ba0c64412964
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-24 21:52:52 +0300

    Shader: Add getViewRay

--- res/shader/basic_frag.glsl
+++ res/shader/basic_frag.glsl
@@ -7,8 +7,16 @@ uniform vec2  uRes;
 
 out vec4 fragColor;
 
+float CAM_FOV = 65;
+
+vec3 getViewRay(vec2 fragCoord, vec2 resolution, float fov)
+{
+    vec2 xy = fragCoord - resolution * 0.5;
+    float z = resolution.y / tan(radians(fov * 0.5));
+    return normalize(vec3(xy, z));
+}
+
 void main()
 {
-    vec2 uv = gl_FragCoord.xy / uRes.xy;
-    fragColor = vec4(uv, 0.5 + 0.5 * sin(uGT), 1);
+    fragColor = vec4(getViewRay(gl_FragCoord.xy, uRes, CAM_FOV), uGT);
 }
