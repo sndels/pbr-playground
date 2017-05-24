a25d30cd183795f487b083f95a02601cf78adb7a
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-24 23:20:59 +0300

    Shader: Add camOrient

--- res/shader/basic_frag.glsl
+++ res/shader/basic_frag.glsl
@@ -7,7 +7,19 @@ uniform vec2  uRes;
 
 out vec4 fragColor;
 
+// Camera
+vec3  CAM_POS = vec3(0, 0, -3);
+vec3  CAM_TARGET = CAM_POS + vec3(0, 0, 1);
+vec3  CAM_UP = vec3(0, 1, 0);
 float CAM_FOV = 65;
+
+mat3 camOrient(vec3 eye, vec3 target, vec3 up)
+{
+    vec3 n = normalize(target - eye);
+    vec3 u = normalize(cross(up, n));
+    vec3 v = cross(n, u);
+    return mat3(u, v, n);
+}
 
 vec3 getViewRay(vec2 fragCoord, vec2 resolution, float fov)
 {
@@ -18,5 +30,8 @@ vec3 getViewRay(vec2 fragCoord, vec2 resolution, float fov)
 
 void main()
 {
-    fragColor = vec4(getViewRay(gl_FragCoord.xy, uRes, CAM_FOV), uGT);
+    // Calculate view ray direction in scene space
+    vec3 vr = getViewRay(gl_FragCoord.xy, uRes, CAM_FOV);
+    vr = camOrient(CAM_POS, CAM_TARGET, CAM_UP) * vr;
+    fragColor = vec4(vr, uGT);
 }
