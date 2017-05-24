156db83b976ca4945dddafe4f7fca1ddd5baba15
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-24 23:33:08 +0300

    Shader: Add ray marching loop and simple scene

--- res/shader/basic_frag.glsl
+++ res/shader/basic_frag.glsl
@@ -7,6 +7,12 @@ uniform vec2  uRes;
 
 out vec4 fragColor;
 
+// Ray marching
+const int   MAX_MARCHING_STEPS = 256;
+const float MIN_DIST = 0;
+const float MAX_DIST = 10;
+const float EPSILON = 0.0001;
+
 // Camera
 vec3  CAM_POS = vec3(0, 0, -3);
 vec3  CAM_TARGET = CAM_POS + vec3(0, 0, 1);
@@ -28,10 +34,40 @@ vec3 getViewRay(vec2 fragCoord, vec2 resolution, float fov)
     return normalize(vec3(xy, z));
 }
 
+float fScene(vec3 p)
+{
+    vec3 spherePos = vec3(0);
+    return fSphere(p - spherePos, 0.75);
+}
+
+float castRay(vec3 rd, vec3 ro)
+{
+    float depth = MIN_DIST;
+    for (int i = 0; i < MAX_MARCHING_STEPS; ++i) {
+        float dist = fScene(ro + depth * rd);
+        if (dist < EPSILON) break;
+
+        depth += dist;
+
+        if (depth > MAX_DIST) break;
+    }
+    return depth;
+}
+
 void main()
 {
     // Calculate view ray direction in scene space
-    vec3 vr = getViewRay(gl_FragCoord.xy, uRes, CAM_FOV);
-    vr = camOrient(CAM_POS, CAM_TARGET, CAM_UP) * vr;
-    fragColor = vec4(vr, uGT);
+    vec3 rd = getViewRay(gl_FragCoord.xy, uRes, CAM_FOV);
+    rd = camOrient(CAM_POS, CAM_TARGET, CAM_UP) * rd;
+
+    // Cast a ray into scene
+    float depth = castRay(rd, CAM_POS);
+
+    // Check if it missed
+    if (depth > MAX_DIST - EPSILON) {
+        fragColor = vec4(0);
+        return;
+    }
+
+    fragColor = vec4(rd, uGT);
 }
