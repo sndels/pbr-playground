41d8e9bfc43250b2b988ec2fe70224c139b58f51
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-25 22:54:27 +0300

    Shader: Add plane with different material

--- res/shader/basic_frag.glsl
+++ res/shader/basic_frag.glsl
@@ -14,21 +14,23 @@ out vec4 fragColor;
 // Ray marching
 const int   MAX_MARCHING_STEPS = 256;
 const float MIN_DIST = 0;
-const float MAX_DIST = 10;
+const float MAX_DIST = 100;
 const float EPSILON = 0.0001;
 
 // Camera
-vec3  CAM_POS = vec3(0, 0, -3);
-vec3  CAM_TARGET = CAM_POS + vec3(0, 0, 1);
+vec3  CAM_POS = vec3(0, 1, -4);
+vec3  CAM_TARGET = vec3(0, 0, 0);
 vec3  CAM_UP = vec3(0, 1, 0);
 float CAM_FOV = 65;
 
 
 // Material
-const int NUM_MATERIALS = 1;
-vec3      ALBEDO[NUM_MATERIALS] = vec3[](vec3(1, 0, 0));
-float     ROUGHNESS[NUM_MATERIALS] = float[](0.3);
-float     METAL[NUM_MATERIALS] = float[](0);
+const int NUM_MATERIALS = 2;
+int       ACTIVE_MATERIAL = 0;
+vec3      ALBEDO[NUM_MATERIALS] = vec3[](vec3(1, 0, 0),
+                                         vec3(0.2));
+float     ROUGHNESS[NUM_MATERIALS] = float[](0.3, 1);
+float     METAL[NUM_MATERIALS] = float[](0, 0);
 
 mat3 camOrient(vec3 eye, vec3 target, vec3 up)
 {
@@ -47,10 +49,13 @@ vec3 getViewRay(vec2 fragCoord, vec2 resolution, float fov)
 
 float fScene(vec3 p)
 {
-    vec3 spherePos = vec3(0);
-    return fSphere(p - spherePos, 0.75);
+    float sphereDist = fSphere(p - vec3(0), 0.75);
+    float planeDist = fPlane(p, vec3(0, 1, 0), 2);
+    ACTIVE_MATERIAL = sphereDist < planeDist ? 0 : 1;
+    return min(sphereDist, planeDist);
 }
 
+// Don't call this before retrieving material from hit
 vec3 getN(vec3 p)
 {
     vec3 e = vec3(EPSILON, 0, 0);
@@ -65,7 +70,7 @@ float castRay(vec3 rd, vec3 ro)
     float depth = MIN_DIST;
     for (int i = 0; i < MAX_MARCHING_STEPS; ++i) {
         float dist = fScene(ro + depth * rd);
-        if (dist < EPSILON) break;
+        if (dist < depth * EPSILON) break;
 
         depth += dist;
 
@@ -92,11 +97,16 @@ void main()
     // Calculate ray to hit
     vec3 p = CAM_POS + depth * rd;
 
+    // Retrieve material for hit
+    vec3 albedo = ALBEDO[ACTIVE_MATERIAL];
+    float rough = ROUGHNESS[ACTIVE_MATERIAL];
+    float metal = METAL[ACTIVE_MATERIAL];
+
     // Directions to lights from hit
     vec3 lVecs[NUM_LIGHTS];
     for (int i = 0; i < NUM_LIGHTS; ++i) lVecs[i] = normalize(LIGHT_POS[i] - p);
 
     // Evaluate final shading
     fragColor = vec4(evalLighting(-rd, getN(p), lVecs, LIGHT_INT,
-                                  ALBEDO[0], ROUGHNESS[0], METAL[0]), 1);
+                                  albedo, rough, metal), 1);
 }
