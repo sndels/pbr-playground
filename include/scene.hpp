08a625aaeb1b0d2613cacd1427986defc5201730
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-30 22:29:29 +0300

    Add Scene

---
+++ include/scene.hpp
@@ -0,0 +1,30 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include <sync.h>
+#include <vector>
+
+#include "shaderProgram.hpp"
+
+class Scene
+{
+public:
+    // Expects shaders to be vert, frag, (geom)
+    Scene(const std::vector<std::string>& shaders,
+          const std::vector<std::string>& syncUniforms, sync_device* rocket);
+    ~Scene() {}
+
+    void bind(double syncRow);
+    void reload();
+    bool shaderLinked();
+    GLint getULoc(const std::string& uniform);
+
+private:
+    ShaderProgram                  _shaderProg;
+    std::vector<std::string>       _uniforms;
+    std::vector<GLint>             _uLocations;
+    std::vector<const sync_track*> _syncTracks;
+
+};
+
+#endif // SCENE_HPP
---
+++ src/scene.cpp
@@ -0,0 +1,38 @@
+#include "scene.hpp"
+
+Scene::Scene(const std::vector<std::string>& shaders,
+             const std::vector<std::string>& syncUniforms, sync_device* rocket) :
+    _shaderProg(shaders[0], shaders[1], shaders.size() > 2 ? shaders[2] : "")
+{
+    for (const auto& u : syncUniforms) {
+        _uniforms.emplace_back(u);
+        _uLocations.emplace_back(_shaderProg.getULoc(u));
+        _syncTracks.emplace_back(sync_get_track(rocket, u.c_str()));
+    }
+}
+
+GLint Scene::getULoc(const std::string& uniform)
+{
+    return _shaderProg.getULoc(uniform);
+}
+
+void Scene::bind(double syncRow)
+{
+    _shaderProg.bind();
+    for (auto i = 0u; i < _uLocations.size(); ++i)
+        glUniform1f(_uLocations[i], (float)sync_get_val(_syncTracks[i], syncRow));
+}
+
+void Scene::reload()
+{
+    if (_shaderProg.reload()) {
+        _uLocations.clear();
+        for (const auto& u : _uniforms)
+            _uLocations.emplace_back(_shaderProg.getULoc(u));
+   }
+}
+
+bool Scene::shaderLinked()
+{
+    return _shaderProg.isLinked();
+}
