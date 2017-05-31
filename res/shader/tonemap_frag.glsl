b120e8691245d6ffb2763d94b1ed04f7dd215b03
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-31 22:41:49 +0300

    Shader: Add tonemap_frag

---
+++ res/shader/tonemap_frag.glsl
@@ -0,0 +1,21 @@
+#version 410
+
+#include "uniforms.glsl"
+
+// Inputs
+uniform float uExposure;
+
+// Hdr-input
+uniform sampler2D uHdrSampler;
+
+// Output
+out vec4 fragColor;
+
+void main()
+{
+    const float gamma = 2.2;
+    vec3 hdr = texture(uHdrSampler, gl_FragCoord.xy / uRes).rgb;
+    vec3 mapped = vec3(1) - exp(-hdr * uExposure);
+    mapped = pow(mapped, vec3(1 / gamma));
+    fragColor = vec4(mapped, 1);
+}
