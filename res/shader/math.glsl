1b04ae5417a9f667d12f81ed2f6f339f638841bd
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-05-31 18:48:54 +0300

    Shader: Add rotations around axis

---
+++ res/shader/math.glsl
@@ -0,0 +1,20 @@
+vec3 rotateX(vec3 p, float rad)
+{
+    float c = cos(rad);
+    float s = sin(rad);
+    return vec3(p.x, c * p.y - s * p.z, s * p.y + c * p.z);
+}
+
+vec3 rotateY(vec3 p, float rad)
+{
+    float c = cos(rad);
+    float s = sin(rad);
+    return vec3(c * p.x + s * p.z, p.y, -s * p.x + c * p.z);
+}
+
+vec3 rotateZ(vec3 p, float rad)
+{
+    float c = cos(rad);
+    float s = sin(rad);
+    return vec3(c * p.x - s * p.y, s * p.x + c * p.y, p.z);
+}
