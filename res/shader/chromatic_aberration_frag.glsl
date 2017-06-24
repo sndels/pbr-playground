06c0ac7c5bad857875ca72214944123ed235f0c6
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-06-24 12:11:19 +0300

    Shader: Only apply ca if active

--- res/shader/chromatic_aberration_frag.glsl
+++ res/shader/chromatic_aberration_frag.glsl
@@ -15,16 +15,24 @@ layout (location = 0) out vec3 colorBuffer;
 void main()
 {
     vec2 texCoord = gl_FragCoord.xy / uRes;
-    vec2 dir = 0.5 - texCoord;
-    vec2 caOffset = dir * uCAberr * 0.1;
-    vec2 blurStep = caOffset * 0.06;
 
-    vec3 sum = vec3(0);
-    for (int i = 0; i < SAMPLES; i++) {
-        sum += vec3(texture(uColorSampler, texCoord + caOffset + i * blurStep).r,
-                    texture(uColorSampler, texCoord + i * blurStep).g,
-                    texture(uColorSampler, texCoord - caOffset + i * blurStep).b);
+    // Only sample if effect is active
+    if (uCAberr > 0) {
+        vec2 dir = 0.5 - texCoord;
+        vec2 caOffset = dir * uCAberr * 0.1;
+        vec2 blurStep = caOffset * 0.06;
+
+        vec3 sum = vec3(0);
+        // TODO: This is expensive
+        for (int i = 0; i < SAMPLES; i++) {
+            sum += vec3(texture(uColorSampler, texCoord + caOffset + i * blurStep).r,
+                        texture(uColorSampler, texCoord + i * blurStep).g,
+                        texture(uColorSampler, texCoord - caOffset + i * blurStep).b);
+        }
+
+        colorBuffer = sum / SAMPLES;
+    } else {
+        colorBuffer = texture(uColorSampler, texCoord).rgb;
     }
 
-    colorBuffer = sum / SAMPLES;
 }
